8b98b342d03e7442e810c7332fa7b727e79275a5
Zulfeqa <99878199+Zulfeqa@users.noreply.github.com>
2022-09-07 14:08:49 +0200

    Initial commit

---
+++ src/Helloword.java
@@ -0,0 +1,9 @@
+public class Helloword {
+    public static void main(String[] args){
+        System.out.println("Hello word");
+        
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
